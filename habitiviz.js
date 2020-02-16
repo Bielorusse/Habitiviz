805895395b1876708d3d45010934ead562850b08
Bielorusse <thibautvoirand@gmail.com>
2020-02-16 12:47:21 +0100

    added Graph and Cell classes

--- habitiviz.js
+++ habitiviz.js
@@ -6,6 +6,68 @@ Script for Habitiviz.
 const canvas_width = 500;
 const canvas_height = 100;
 
+class Graph {
+    /*
+    Class defining a graph.
+    */
+
+    constructor() {
+        this.cells = [];
+        this.cell_size = 8;
+        this.cells_spacing = 10;
+    }
+
+    draw(sketch) {
+        /*
+        Draws this graph on a given sketch.
+        Input:
+            -sketch     p5js sketch object
+        */
+        for (let cell of this.cells) {
+            sketch.fill(cell.color);
+            sketch.square(cell.x, cell.y, this.cell_size);
+        }
+    }
+}
+
+class Cell {
+    /*
+    Class defining a cell in a graph.
+    */
+
+    constructor(x, y, color) {
+        /*
+        Constructor for the Cell class.
+        Input:
+            -x      int
+            -y      int
+            -color  [int, int, int]
+        */
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
+
+// creating graph showing total activities
+let total_graph = new Graph();
+
+// filling total_graph with dummy values for now
+for (let y = 9; y >= 0; y--) {
+    for (let x = 49; x >= 0; x--) {
+        let cell_id = x + y * 50;
+        let cell_total = 500;
+        let color = Math.floor(255 * cell_id / cell_total);
+        total_graph.cells.push(
+            new Cell(
+                x * total_graph.cells_spacing, // new cell X pos
+                y * total_graph.cells_spacing, // new cell y pos
+                [color, color, color] // new cell color
+            )
+        );
+    }
+}
+
 const s = sketch => {
     /*
     Function which takes a "sketch" object as argument and attaches properties such as setup and
@@ -18,7 +80,7 @@ const s = sketch => {
         Processing setup function.
         Called one time at start.
         */
-        var canvas = sketch.createCanvas(canvas_width, canvas_height);
+        let canvas = sketch.createCanvas(canvas_width, canvas_height);
         canvas.parent(habitiviz_canvas_div);
     };
 
@@ -31,15 +93,8 @@ const s = sketch => {
         sketch.background(0);
         sketch.noStroke();
 
-        for (let row = 0; row < 10; row++) {
-            for (let col = 0; col < 50; col++) {
-                let cell_id = col + row * 50;
-                let cell_total = 500;
-                let color = Math.floor(255 * cell_id / cell_total);
-                sketch.fill(color);
-                sketch.square(col * 10, row * 10, 8);
-            }
-        }
+        // drawing graph
+        total_graph.draw(sketch);
     };
 };
 
