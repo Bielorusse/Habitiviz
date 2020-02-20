6e59b7d4704f6a61f60ee8f3a1e1232b76683a1f
Bielorusse <thibautvoirand@gmail.com>
2020-02-20 21:19:24 +0100

    changed config and added archive dir

--- scripts/fetch_habitica_history.py
+++ scripts/fetch_habitica_history.py
@@ -2,21 +2,28 @@
 Script to fetch habitica history data.
 """
 
+import os
 import json
 import datetime
 import subprocess
+
 
 def main():
     """
     Fetch habitica history data and store it in archive.
     """
 
-    # read credentials
-    with open("config/habitica_api_credentials.json", "r") as infile:
-        credentials = json.load(infile)
+    # read config
+    with open("config/config.json", "r") as infile:
+        config = json.load(infile)
 
     # create output file name
-    output_file = "data/habitica_tasks_history_{}.csv".format(datetime.datetime.now().strftime("%Y%m%d"))
+    output_file = os.path.join(
+        config["archive_dir"],
+        "habitica_tasks_history_{}.csv".format(
+            datetime.datetime.now().strftime("%Y%m%d")
+        ),
+    )
 
     # fetch history data from habitica
     command = [
@@ -27,11 +34,11 @@ def main():
         "GET",
         "--compressed",
         "-H",
-        "x-api-user:{}".format(credentials["id"]),
+        "x-api-user:{}".format(config["id"]),
         "-H",
-        "x-api-key:{}".format(credentials["key"]),
+        "x-api-key:{}".format(config["key"]),
         "--output",
-        output_file
+        output_file,
     ]
     subprocess.call(command)
 
