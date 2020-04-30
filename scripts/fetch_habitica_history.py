e1ecd8ba714a04f3d048939f121158aabb638be6
Bielorusse <thibautvoirand@gmail.com>
2020-04-30 21:13:15 +0200

    changed fetch_habitica_history script to copy current week data in single archive file

--- scripts/fetch_habitica_history.py
+++ scripts/fetch_habitica_history.py
@@ -1,5 +1,5 @@
 """
-Script to fetch habitica history data of the current week.
+Script to fetch habitica history data of the current week and add it to the local archive.
 """
 
 import os
@@ -18,8 +18,8 @@ def main():
     with open(os.path.join(habitiviz_path, "config", "config.json"), "r") as infile:
         config = json.load(infile)
 
-    # create output file name
-    output_file = os.path.join(
+    # create current history file name
+    current_file = os.path.join(
         config["archive_dir"],
         "habitica_tasks_history_{}.csv".format(
             datetime.datetime.now().strftime("%Y%m%d")
@@ -39,14 +39,19 @@ def main():
         "-H",
         "x-api-key:{}".format(config["key"]),
         "--output",
-        output_file,
+        current_file,
     ]
     subprocess.call(command)
 
-    # copy only data from the current week in a tmp file
-    tmp_file = "{}_tmp.csv".format(os.path.splitext(output_file)[0])
-    with open(output_file, "r") as infile:
-        with open(tmp_file, "w") as outfile:
+    # copy only data from the current week in the archive file
+    archive_file = os.path.join(
+        config["archive_dir"],
+        "habitica_tasks_history.csv"
+    )
+    with open(current_file, "r") as infile:
+        with open(archive_file, "a") as outfile:
+
+
             for row in [r for r in infile if not r == "" and not r.startswith("Task Name")]:
 
                 date_str = row.split(",")[3][:10] # parse date from history file
@@ -54,10 +59,6 @@ def main():
                 if date_obj.isocalendar()[1] == datetime.datetime.now().isocalendar()[1]:
                     # if the item is within the current week, copy it in the tmp file
                     outfile.write(row)
-
-    # replace the archive file with the tmp file
-    os.remove(output_file)
-    os.rename(tmp_file, output_file)
 
 if __name__ == "__main__":
 
